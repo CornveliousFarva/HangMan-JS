2809167f73ca5bb9c5ce18de544538b2117626d6
Greg Schuman <gcschuman@hotmail.com>
2020-08-09 15:09:18 -0700

    Added functions that deal with wrong letters.

--- script.js
+++ script.js
@@ -36,4 +36,33 @@ function displayWord() {
       popup.style.display = 'flex';
     }
   }
+
+  // Update the wrong letters
+function updateWrongLettersEl() {
+    // Display wrong letters
+    wrongLettersEl.innerHTML = `
+      ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+      ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
   
+    // Display parts
+    figureParts.forEach((part, index) => {
+      const errors = wrongLetters.length;
+  
+      if (index < errors) {
+        part.style.display = 'block';
+      } else {
+        part.style.display = 'none';
+      }
+    });
+  
+    // Check if lost
+    if (wrongLetters.length === figureParts.length) {
+      finalMessage.innerText = 'Unfortunately you lost. ðŸ˜•';
+      popup.style.display = 'flex';
+    }
+  }
+  
+
+  
+displayWord();
